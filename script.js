5c715735733cdcc63fc77de819d3f3cfcb15ad60
off-art <art.dev.k@gmail.com>
2020-05-25 17:06:22 +0300

    calc done

---
+++ script.js
@@ -0,0 +1,21 @@
+let vue = new Vue({
+  el: "#app",
+  data: {
+    result: "",
+    nums: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    oper: ["+", "-", "*", "/"],
+  },
+  methods: {
+    input: function (char) {
+      this.result = this.result.toString();
+      this.result += char;
+      
+    },
+    reset: function () {
+      this.result = "";
+    },
+    calc: function () {
+      this.result = eval(this.result);
+    },
+  },
+});
